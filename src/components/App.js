d94a51e94ce10443ce294ad4402b7824913da96d
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-01-26 09:32:36 -0500

    commit tree f1af6db8b75a43a17bcedb39372344c4ceefac39

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,51 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
 
 function App() {
   const [pets, setPets] = useState([]);
-  const [filters, setFilters] = useState({ type: "all" });
+  const [filters, setFilters] = useState("all");
+
+  function onAdoptPet(id){
+    const adopted = pets.map((pet)=>{
+      if(pet.id === id){
+        pet.isAdopted = true;
+        return pet;
+      }
+      else {
+        return pet;
+      }
+    })
+    setPets(adopted);
+  }
+
+  function onChangeType(filter){
+    setFilters(filter)
+  }
+
+  function onFindPetsClick(){
+      if(filters === 'all'){
+        fetch('http://localhost:3001/pets')
+        .then((resp)=>resp.json())
+        .then((pets)=>setPets(pets))
+      }
+      else if(filters === 'cat'){
+        fetch('http://localhost:3001/pets?type=cat')
+        .then((resp)=>resp.json())
+        .then((pets)=>setPets(pets))
+      }
+      else if(filters === 'dog'){
+        fetch('http://localhost:3001/pets?type=dog')
+        .then((resp)=>resp.json())
+        .then((pets)=>setPets(pets))
+      }
+      else if(filters === 'micropig'){
+        fetch('http://localhost:3001/pets?type=micropig')
+        .then((resp)=>resp.json())
+        .then((pets)=>setPets(pets))
+      }
+  }
 
   return (
     <div className="ui container">
@@ -15,10 +55,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={onChangeType} onFindPetsClick={onFindPetsClick}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet}/>
           </div>
         </div>
       </div>
